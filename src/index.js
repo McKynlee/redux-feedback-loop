0f4e9cf40f96907948bb94e8480b2038c09fccb5
McKynlee Westman <mckynlee.westman@gmail.com>
2021-02-25 18:17:36 -0600

    Redux brought in

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Bring in redux:
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+
+const store = createStore(
+  combineReducers({
+
+  }),
+  applyMiddleware(logger)
+)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
