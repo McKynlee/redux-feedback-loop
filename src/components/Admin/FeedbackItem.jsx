9ddd888ded8793dd22bb9b58cb0d82686bf288e3
McKynlee Westman <mckynlee.westman@gmail.com>
2021-02-27 12:00:56 -0600

    Test conditional rendering is shown in App

--- src/components/Admin/FeedbackItem.jsx
+++ src/components/Admin/FeedbackItem.jsx
@@ -1,37 +1,29 @@
 import axios from "axios";
 import swal from 'sweetalert';
-import { useEffect } from 'react';
-
 
 function FeedbackItem({ userFeedbackRow,
   fetchHistoricalFeedback }) {
   // Capture boolean value from db "flagged" column
   // to use in conditional rendering:
   let flaggedStatus = userFeedbackRow.flagged;
-  console.log('flaggedStatus:', userFeedbackRow.comments, flaggedStatus);
+  // console.log('flaggedStatus:', userFeedbackRow.comments, flaggedStatus);
 
   // Create variable to carry conditional JSX generated
   // from handleFlagForReview down to return(...JSX):
   let renderingForFlaggedStatus;
 
-  // on page load, determine how to render flagged table data:
-  useEffect(() => {
-    setFlagRendering();
-  }, []);
-
   // Control how the table data will be rendered for 'Flag for review'
   // so that when flaggedStatus is true, it will show that the admin has 
   // flagged that feedback for review
-  const setFlagRendering = () => {
-    switch (flaggedStatus) {
-      case false:
-        renderingForFlaggedStatus = 'FALSE!';
-        break;
-      case true:
-        renderingForFlaggedStatus = 'TRUE!'
-        break;
-    }
-  } // end handleFlagForReview
+  switch (flaggedStatus) {
+    case false:
+      renderingForFlaggedStatus =
+        <button>Flag</button>;
+      break;
+    case true:
+      renderingForFlaggedStatus = 'TRUE!';
+      break;
+  }
 
   // Grab id for feedback row and delete when button clicked:
   const handleDelete = (feedbackToDeleteId) => {
