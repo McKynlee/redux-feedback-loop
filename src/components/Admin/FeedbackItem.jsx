dd3d400e5f0a34c05f4df284feb29feafbb9839b
McKynlee Westman <mckynlee.westman@gmail.com>
2021-02-27 08:44:18 -0600

    Delete button responding on click with corresponding id

--- src/components/Admin/FeedbackItem.jsx
+++ src/components/Admin/FeedbackItem.jsx
@@ -1,5 +1,12 @@
 
 function FeedbackItem({ userFeedbackRow }) {
+
+  // Grab id for feedback row and delete when button clicked:
+  const handleDelete = (feedbackToDeleteId) => {
+    console.log('handleDelete:', feedbackToDeleteId);
+  }
+
+  // Render row of data for each individual user input:
   return (
     <tr className="admin-table">
       <td className="admin-table">{userFeedbackRow.feeling}</td>
@@ -7,7 +14,9 @@ function FeedbackItem({ userFeedbackRow }) {
       <td className="admin-table">{userFeedbackRow.support}</td>
       <td className="admin-table">{userFeedbackRow.comments}</td>
       <td className="admin-table">Flag icon</td>
-      <td className="admin-table"></td>
+      <td className="admin-table">
+        <button onClick={() => handleDelete(userFeedbackRow.id)}>DELETE</button>
+      </td>
     </tr>
   )
 } // end FeedbackItem
