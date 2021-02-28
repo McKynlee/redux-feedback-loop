c88dc887724ed0734c50a25ebd65e50946910441
McKynlee Westman <mckynlee.westman@gmail.com>
2021-02-28 16:04:05 -0600

    styling Review Page

--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -1,7 +1,14 @@
 import { useHistory, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
-import { Button, Box } from '@material-ui/core';
+import {
+  Button,
+  Typography,
+  Box,
+  Card,
+  CardActions,
+  CardContent
+} from '@material-ui/core';
 
 // Component to display upon hitting NEXT in FormFour:
 function ReviewPage() {
@@ -36,43 +43,53 @@ function ReviewPage() {
   } // end submitToSuccess
 
   return (
-    <div>
-      <h2>Review Your Answers:</h2>
-      {/* // Display data captured from user's input for review: */}
-      <table className="review-feedback" >
-        <tbody>
-          <tr>
-            <td>
-              <Link to="/edit1">How you're feeling: {currentUserFeedback.feeling}</Link>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Link to="/edit2">How you're understanding the material: {currentUserFeedback.understanding}</Link>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Link to="/edit3">How well you feel supported: {currentUserFeedback.support}</Link>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Link to="/edit4">Additional comments: {currentUserFeedback.comments}</Link>
-            </td>
-          </tr>
-          <tr>
-            <td>
+    <div className="card-wrapper">
+      <Box boxShadow={3}
+        className="card-container"
+      >
+        <Card variant="outlined">
+          <CardContent>
+            <Typography variant="h5" component="h2"
+              gutterBottom>
+              Review Your Answers:
+            </Typography>
+            {/* // Display data captured from user's input for review: */}
+            <table className="review-feedback" >
+              <tbody>
+                <tr>
+                  <td>
+                    <Link to="/edit1">
+                      How you're feeling: {currentUserFeedback.feeling}</Link>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <Link to="/edit2">How you're understanding the material: {currentUserFeedback.understanding}</Link>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <Link to="/edit3">How well you feel supported: {currentUserFeedback.support}</Link>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <Link to="/edit4">Additional comments: {currentUserFeedback.comments}</Link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <CardActions className="card-actions">
               <Box m={3}>
                 <Button variant="contained" color="primary"
                   onClick={submitToSuccess}>
                   Submit Feedback
-              </Button>
+                      </Button>
               </Box>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+            </CardActions>
+          </CardContent>
+        </Card>
+      </Box>
     </div>
   )
 } // end ReviewPage
